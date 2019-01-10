e69aff49405946d8da9d3f3378b04dfb8ba3dba8
scaunois <sebastiencaunois@gmail.com>
2019-01-10 14:29:18 +0100

    refactoring the loop

--- FrequencyTest.java
+++ FrequencyTest.java
@@ -16,32 +16,36 @@ class FrequencyTest {
 
 	public static void main(String a[]) {
 		
-		String str, word;
+		String textLine;
+		String word;
 		String choice;
 		Scanner sc = new Scanner(System.in);
 		do {
+			
 			System.out.println("1. Check ");
 			System.out.println("2. Exit");
 			System.out.println("Enter Your Choice : ");
 			choice = sc.next();
 			sc.nextLine();
+			
 			if (choice.equals("1")) {
-				System.out.println(
-						"Enter Sentence : (** Kindly give a space before and after '.' for accurate results) ");
-				str = sc.nextLine();
-				System.out.println("Enter the word whoose frequency you wanna check - ");
+				
+				System.out.println("Enter Sentence : (** Kindly give a space before and after '.' for accurate results)");
+				textLine = sc.nextLine();
+				System.out.println("Enter the word whose frequency you wanna check - ");
 				word = sc.nextLine();
-				System.out.println("Count = " + frequency(str, word));
+				System.out.println("Count = " + frequency(textLine, word));
 				System.out.println();
-			} else if (choice.equals("2")) {
-				System.out.println("Exiting...");
-				System.exit(0);
-			} else {
+			
+			} else if (!choice.matches("1|2")) {
 				System.out.println("Invalid Choice !! Re-enter ");
-				continue;
 			}
-		} while (true);
-		//sc.close();
+			
+		} while (!choice.equals("2"));
+		
+		System.out.println("Exiting...");
+		sc.close();
+		System.exit(0);
 	
 	}
 
